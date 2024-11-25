a2a091007cb896fc56c1bc82f207205bd6b67c38
Elis Santos <132396943+elis-s@users.noreply.github.com>
2024-11-25 19:07:41 -0300

    Add files via upload

---
+++ MenorMaior/src/main/java/menormaior/MenorMaior.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package menormaior;
+
+/*) Escreva um aplicativo que insira três inteiros digitados pelo usuário e exiba a soma, média, produto e os
+números menores e maiores. Utilize as técnicas mostradas na Figura 2.15. [Observação: o cálculo da média neste exercício deve resultar
+em uma representação de inteiro. Assim, se a soma dos valores for 7, a média deverá ser 2, não 2,3333...]
+ */
+public class MenorMaior {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
