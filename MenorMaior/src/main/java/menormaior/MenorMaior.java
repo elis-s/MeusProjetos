49efe13ba570af4ee77a8e767a3b1295f7da71de
Elis Santos <132396943+elis-s@users.noreply.github.com>
2024-11-25 19:20:13 -0300

    Add files via upload

--- MenorMaior/src/main/java/menormaior/MenorMaior.java
+++ MenorMaior/src/main/java/menormaior/MenorMaior.java
@@ -4,13 +4,44 @@
 
 package menormaior;
 
+import java.util.Scanner;
+
 /*) Escreva um aplicativo que insira três inteiros digitados pelo usuário e exiba a soma, média, produto e os
-números menores e maiores. Utilize as técnicas mostradas na Figura 2.15. [Observação: o cálculo da média neste exercício deve resultar
-em uma representação de inteiro. Assim, se a soma dos valores for 7, a média deverá ser 2, não 2,3333...]
+números menores e maiores.
  */
 public class MenorMaior {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite o primeiro numero: ");
+        int num1 = teclado.nextInt();
+        
+        System.out.println("Digite o segundo numero: ");
+        int num2 = teclado.nextInt();
+        
+        System.out.println("Digite o terceiro numero: ");
+        int num3 = teclado.nextInt();
+        
+        int soma = num1 + num2 + num3;
+        System.out.println("Soma: " + soma);
+        
+        int media = soma / 3;
+        System.out.println("Media: " + media);
+        
+        int produto = num1 * num2 * num3;
+        System.out.println("Produto: " + produto);
+        
+        if(num1 > num2 && num1 > num3){
+            System.out.println("O primeiro numero é maior");
+        }else if(num1 < num2 && num2 > num3){
+            System.out.println("O segundo numero é maior");
+        }else if(num3 > num1 && num3 > num2){
+            System.out.println("O terceiro numero é maior");
+        }else{
+            System.out.println("INVÁLIDO");
+        }
+
     }
 }
