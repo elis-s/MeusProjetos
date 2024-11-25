6f0831eff72822525e91467a367586af9d1f21f6
Elis Santos <132396943+elis-s@users.noreply.github.com>
2024-11-25 20:25:26 -0300

    Add files via upload

---
+++ TabuleiroDamasAsterisco/src/main/java/tabuleirodamasasterisco/TabuleiroDamasAsterisco.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tabuleirodamasasterisco;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TabuleiroDamasAsterisco {
+
+    public static void main(String[] args) {
+        
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+        
+    }
+}
