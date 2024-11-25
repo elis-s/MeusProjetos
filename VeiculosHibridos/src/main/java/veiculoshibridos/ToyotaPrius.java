97b7a159db9d49336af8b344cfaf83d27d082444
Elis Santos <132396943+elis-s@users.noreply.github.com>
2024-11-25 19:06:57 -0300

    Add files via upload

---
+++ VeiculosHibridos/src/main/java/veiculoshibridos/ToyotaPrius.java
@@ -0,0 +1,148 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package veiculoshibridos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ToyotaPrius {
+    
+    public int desempenhoUrbano;
+    public int desempenhoEstrada;
+    public String tipoMotor;
+    public int potencia;
+    public String tipoBateria;
+    public int capacidadeBateria;
+    public int pesoBateria;
+    public String autonomiaEletrica;
+    public int emissaoCO2;
+    
+    public ToyotaPrius (int desempenhoUrbano, int desempenhoEstrada, String tipoMotor, int potencia, String tipoBateria, int capacidadeBateria, int pesoBateria, String autonomiaEletrica, int emissaoCO2){
+        
+        this.desempenhoUrbano = desempenhoUrbano;
+        this.desempenhoEstrada = desempenhoEstrada;
+        this.tipoMotor = tipoMotor;
+        this.potencia = potencia;
+        this.tipoBateria = tipoBateria;
+        this.capacidadeBateria = capacidadeBateria;
+        this.pesoBateria = pesoBateria;
+        this.autonomiaEletrica = autonomiaEletrica;
+        this.emissaoCO2 = emissaoCO2;
+        
+        
+    }
+    
+    public int getDesempenhoUrbano(){
+        
+        return 23-26;
+        
+    }
+    
+    public int getDesempenhoEstrada(){
+        
+        return desempenhoUrbano;
+        
+    }
+    
+    public String getTipoMotor(){
+        
+        return tipoMotor;
+        
+    }
+    
+    public int getPotencia(){
+        
+        return potencia;
+        
+    }
+    
+    public String getTipoBateria(){
+        
+        return tipoBateria;
+        
+    }
+    
+    public int getCapacidadeBateria(){
+        
+        return capacidadeBateria;
+        
+    }
+    
+    public int getPesoBateria(){
+        
+        return pesoBateria;
+        
+    }
+    
+    public String getAutonomiaEletrica(){
+    
+        return autonomiaEletrica;
+    
+    }
+    
+    public int getEmissaoCO2(){
+        
+        return emissaoCO2;
+        
+    }
+    
+    public void setDesempenhoUrbano(){
+        
+        this.desempenhoUrbano = desempenhoUrbano;
+     
+    }
+    
+    public void setDesempenhoEstrada(){
+        
+        this.desempenhoEstrada = desempenhoEstrada;
+        
+    }
+    
+    public void setTipoMotor(){
+        
+        this.tipoMotor = tipoMotor;
+        
+    }
+    
+    public void setPotencia(){
+        
+        this.potencia = potencia;
+        
+    }
+    
+    public void setTipoBateria(){
+        
+        this.tipoBateria = tipoBateria;
+        
+    }
+ 
+    public void setCapacidadeBateria(){
+        
+        this.capacidadeBateria = capacidadeBateria;
+        
+    }
+    
+    public void setPesoBateria(){
+        
+        this.pesoBateria = pesoBateria;
+        
+    }
+    
+    public void setAutonomiaEletrica(){
+        
+        this.autonomiaEletrica = autonomiaEletrica;
+        
+    }
+ 
+    public void setEmissaoCO2(){
+        
+        this.emissaoCO2 = emissaoCO2;
+        
+    }
+    
+    
+    
+}
---
+++ VeiculosHibridos/src/main/java/veiculoshibridos/VeiculosHibridos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package veiculoshibridos;
+
+import java.util.Scanner;
+
+/*
+
+Agora você começará a estabelecer os aspectos de uma classe 
+chamada “veículos híbridos”. Eles estão se tornando cada vez mais populares, porque muitas vezes têm
+um desempenho por litro de combustível muito melhor do que veículos que só utilizam gasolina. Navegue pela web e estude os recursos de
+quatro ou cinco carros híbridos atuais mais populares, e então liste o maior número possível de atributos relacionados a eles. Alguns mais
+comuns incluem desempenho urbano por litro de combustível e desempenho em estradas. Liste também as particularidades das baterias
+(tipo, peso etc.).
+
+ */
+public class VeiculosHibridos {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner (System.in);
+       
+
+    }
+}
