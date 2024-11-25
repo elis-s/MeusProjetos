ad275f751711b4f8c77af66b44d66cb685225937
Elis Santos <132396943+elis-s@users.noreply.github.com>
2024-11-25 19:39:21 -0300

    Add files via upload

---
+++ ExibindoFormasComAsteriscos/src/main/java/exibindoformascomasteriscos/ExibindoFormasComAsteriscos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exibindoformascomasteriscos;
+
+/*(Exibindo formas com asteriscos) Escreva um aplicativo que exiba uma caixa, uma elipse, uma seta e um losango utilizando asteriscos
+(*), como na descrição do livro
+
+ */
+public class ExibindoFormasComAsteriscos {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("****************************");
+        System.out.println(" ******    ***      *       *                  ");
+        System.out.println(" *    * *      *   ***     * *            ");
+        System.out.println(" *    * *       * *****  *    *               ");
+        System.out.println(" *    * *       *   *   *      *          ");
+        System.out.println(" *    * *       *   *   *      *         ");
+        System.out.println(" *    * *       *   *   *      *       ");
+        System.out.println(" ****** *       *   *    *    *   ");
+        System.out.println("         *     *    *     *  *       ");
+        System.out.println("           ***              *          ");
+        System.out.println("******************************");
+        
+
+        
+    }
+}
