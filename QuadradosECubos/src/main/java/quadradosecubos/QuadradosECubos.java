520a5fb345b71da6e378a94bdc2381f3ec694a44
Elis Santos <132396943+elis-s@users.noreply.github.com>
2024-11-26 20:39:02 -0300

    Add files via upload

---
+++ QuadradosECubos/src/main/java/quadradosecubos/QuadradosECubos.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package quadradosecubos;
+
+/*Escreva um aplicativo que calcule os quadrados e cubos dos números de 0 a 10 e imprima 
+os valores resultantes em formato de tabela como a seguir:
+ */
+public class QuadradosECubos {
+
+    public static void main(String[] args) {
+
+        int num0 = 0;
+        int quadrado0 = num0 * num0;
+        int cubo0 = num0 * num0 * num0;
+        
+        int num1 = 1;
+        int quadrado1 = num1 * num1;
+        int cubo1 = num1 * num1 * num1;
+        
+        int num2 = 2;
+        int quadrado2 = num2 * num2;
+        int cubo2 = num2 * num2 * num2;
+        
+        int num3 = 3;
+        int quadrado3 = num3 * num3;
+        int cubo3 = num3 * num3 * num3;
+        
+        int num4 = 4;
+        int quadrado4 = num4 * num4;
+        int cubo4 = num4 * num4 * num4;
+        
+        int num5 = 5;
+        int quadrado5 = num5 * num5;
+        int cubo5 = num5 * num5 * num5;
+        
+        int num6 = 6;
+        int quadrado6 = num6 * num6;
+        int cubo6 = num6 * num6 * num6;
+        
+        int num7 = 7;
+        int quadrado7 = num7 * num7;
+        int cubo7 = num7 * num7 * num7;
+        
+        int num8 = 8;
+        int quadrado8 = num8 * num8;
+        int cubo8 = num8 * num8 * num8;
+        
+        int num9 = 9;
+        int quadrado9 = num9 * num9;
+        int cubo9 = num9 * num9 * num9;
+        
+        int num10 = 10;
+        int quadrado10 = num10 * num10;
+        int cubo10 = num10 * num10 * num10;
+        
+        System.out.println("NUMEROS");
+        System.out.println(num0);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(num7);
+        System.out.println(num8);
+        System.out.println(num9);
+        System.out.println(num10);
+        
+        System.out.println("QUADRADO");
+        System.out.println(quadrado0);
+        System.out.println(quadrado1);
+        System.out.println(quadrado2);
+        System.out.println(quadrado3);
+        System.out.println(quadrado4);
+        System.out.println(quadrado5);
+        System.out.println(quadrado6);
+        System.out.println(quadrado7);
+        System.out.println(quadrado8);
+        System.out.println(quadrado9);
+        System.out.println(quadrado10);
+        
+        System.out.println("CUBO");
+        System.out.println(cubo0);
+        System.out.println(cubo1);
+        System.out.println(cubo2);
+        System.out.println(cubo3);
+        System.out.println(cubo4);
+        System.out.println(cubo5);
+        System.out.println(cubo6);
+        System.out.println(cubo7);
+        System.out.println(cubo8);
+        System.out.println(cubo9);
+        System.out.println(cubo10);
+    }
+}
